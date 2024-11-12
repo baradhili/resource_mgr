fd26f54fd423081eb827882fa210597d9dcac4a9
bret <bret@ticm.com>
2024-11-12 05:49:39 +0000

    tweaked laravel-dashboard to allow inclusion in the content frame

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,35 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
+                widgets:
+                <ul>
+                    <li>Average Workload</li>
+                    <li>Contractor Tenure Tracker</li>
+                    <li>Skill Set Coverage and Gaps</li>
+                    <li>Unrecovered Resource Cost</li>
+                    <li>Upcoming Demands</li>
+                    <li>Demand Funnel</li>
+                    <li>Resource Availability by Month</li>
+                </ul>
+                <x-dashboard>
+
+                </x-dashboard>
             </div>
-            widgets:
-            <ul>
-                <li>Average Workload</li>
-                <li>Contractor Tenure Tracker</li>
-                <li>Skill Set Coverage and Gaps</li>
-                <li>Unrecovered Resource Cost</li>
-                <li>Upcoming Demands</li>
-                <li>Demand Funnel</li>
-                <li>Resource Availability by Month</li>
-            </ul>
         </div>
     </div>
-</div>
 @endsection
