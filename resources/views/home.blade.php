54685ad1f2219723958974e38acf83eb33ed6212
Bret Watson <bret@ticm.com>
2024-11-13 11:20:30 +0800

    stub hand created dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,35 +5,165 @@
 @endsection
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
+    <div class="container-fluid p-0">
+        <div class="row">
+            <div class="col-12 d-flex">
+                <div class="w-100">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Average Workload</h5>
+                                        </div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
+                                        <div class="col-auto">
+                                            <div class="stat text-primary">
+                                                <i class="align-middle" data-feather="truck"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">2.382</h1>
+                                    <div class="mb-0">
+                                        <span class="text-danger">-3.65%</span>
+                                        <span class="text-muted">Since last week</span>
+                                    </div>
+                                </div>
                             </div>
-                        @endif
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Contractor Tenure Tracker</h5>
+                                        </div>
 
-                        {{ __('You are logged in!') }}
+                                        <div class="col-auto">
+                                            <div class="stat text-primary">
+                                                <i class="align-middle" data-feather="users"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">14.212</h1>
+                                    <div class="mb-0">
+                                        <span class="text-success">5.25%</span>
+                                        <span class="text-muted">Since last week</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Skill Set Coverage and Gaps</h5>
+                                        </div>
+
+                                        <div class="col-auto">
+                                            <div class="stat text-primary">
+                                                <i class="align-middle" data-feather="dollar-sign"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">$21.300</h1>
+                                    <div class="mb-0">
+                                        <span class="text-success">6.65%</span>
+                                        <span class="text-muted">Since last week</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Unrecovered Resource Cost</h5>
+                                        </div>
+
+                                        <div class="col-auto">
+                                            <div class="stat text-primary">
+                                                <i class="align-middle" data-feather="shopping-cart"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">64</h1>
+                                    <div class="mb-0">
+                                        <span class="text-danger">-2.25%</span>
+                                        <span class="text-muted">Since last week</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Average Workload</h5>
+                                        </div>
+
+                                        <div class="col-auto">
+                                            <div class="stat text-primary">
+                                                <i class="align-middle" data-feather="truck"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">2.382</h1>
+                                    <div class="mb-0">
+                                        <span class="text-danger">-3.65%</span>
+                                        <span class="text-muted">Since last week</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Contractor Tenure Tracker</h5>
+                                        </div>
+
+                                        <div class="col-auto">
+                                            <div class="stat text-primary">
+                                                <i class="align-middle" data-feather="users"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">14.212</h1>
+                                    <div class="mb-0">
+                                        <span class="text-success">5.25%</span>
+                                        <span class="text-muted">Since last week</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                widgets:
-                <ul>
-                    <li>Average Workload</li>
-                    <li>Contractor Tenure Tracker</li>
-                    <li>Skill Set Coverage and Gaps</li>
-                    <li>Unrecovered Resource Cost</li>
-                    <li>Upcoming Demands</li>
-                    <li>Demand Funnel</li>
-                    <li>Resource Availability by Month</li>
-                </ul>
-                <x-dashboard>
+            </div>
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
 
-                </x-dashboard>
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+
+                            {{ __('You are logged in!') }}
+                        </div>
+                    </div>
+                    widgets:
+                    <ul>
+
+                        <li>Upcoming Demands</li>
+                        <li>Demand Funnel</li>
+                        <li>Resource Availability by Month</li>
+                    </ul>
+                    <x-dashboard>
+
+                    </x-dashboard>
+                </div>
             </div>
         </div>
-    </div>
-@endsection
+    @endsection
