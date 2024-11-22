5bba74eb33e4a49ab8ce484e22184065cfa8e01b
bret <bret@ticm.com>
2024-11-22 07:32:38 +0000

    create estimates erd

---
+++ database/migrations/2024_11_22_062212_create_estimates_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('estimates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('use_name_as_title');
+            $table->date('expiration_date');
+            $table->string('currency_symbol');
+            $table->string('currency_decimal_separator');
+            $table->string('currency_thousands_separator');
+            $table->boolean('allows_to_select_items');
+            $table->string('tags');
+            $table->string('password');
+            $table->timestamps();
+            $table->float('total_cost');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('estimates');
+    }
+};
