5edaea546e9aa73cca060c7d8fca4dc02d89fd88
Bret Watson <bret@ticm.com>
2026-02-09 18:44:29 +0800

    remove domains
    change default name to practice manager

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Practice Manager') }}</title>
 
     <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Resource Allocation Tool
+                    Practice Manager
                 </div>
             </div>
         </div>
