962e3b4c46222eb4bc1df35c29c70345f458b98d
Shift <shift@laravelshift.com>
2026-02-10 08:49:11 +0000

    Streamline config files

--- config/app.php
+++ config/app.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Facade;
 
 return [
+
     'tenure' => env('APP_TENURE'),
+
     'aliases' => Facade::defaultAliases()->merge([
         // 'Example' => App\Facades\Example::class,
         'Menu' => Spatie\Menu\Laravel\Facades\Menu::class,
